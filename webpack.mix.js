70720345d6be0c2198ea80e765696126c266c69f
Josue Vargas Vasquez <jos.vargasv@gmail.com>
2019-05-26 13:14:58 -0300

    [WIP] | Add postcss plugins.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
-let mix = require("laravel-mix");
+let mix = require("laravel-mix")
 
 mix
   .js("src/js/app.js", "assets/js")
   .postCss("src/css/app.css", "assets/css", [require("tailwindcss")])
-  .setPublicPath("assets");
+  .setPublicPath("assets")
